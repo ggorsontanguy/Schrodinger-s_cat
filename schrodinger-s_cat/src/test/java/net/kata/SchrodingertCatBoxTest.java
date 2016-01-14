ed2824fc5150f3430e430ab7502682fff04fae35
ggorsontanguy <guillaume@gorson-tanguy.net>
2016-01-14 23:59:18 +0100

    REFACTOR test

--- schrodinger-s_cat/src/test/java/net/kata/SchrodingertCatBoxTest.java
+++ schrodinger-s_cat/src/test/java/net/kata/SchrodingertCatBoxTest.java
@@ -13,6 +13,7 @@ public class SchrodingertCatBoxTest {
 
 	@Test
 	public final void may_kill_the_cat_when_open_the_box() {
+		//GIVEN
 		SchrodingertCatBox given = Experimentation.start(new Cat(), new RadioactiveIsotope());
 		assertThat(given).isNotNull();
 		
