2c90d0429d273abce92d5d55c9c956942106b243
ggorsontanguy <guillaume@gorson-tanguy.net>
2016-01-14 23:58:17 +0100

    REFACTOR test

--- schrodinger-s_cat/src/test/java/net/kata/SchrodingertCatBoxTest.java
+++ schrodinger-s_cat/src/test/java/net/kata/SchrodingertCatBoxTest.java
@@ -1,22 +1,20 @@
 package net.kata;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 import java.util.function.BiFunction;
 
 import org.junit.Test;
+
 
 public class SchrodingertCatBoxTest {
 
 
 	@Test
 	public final void may_kill_the_cat_when_open_the_box() {
-		//GIVEN
-		
-		BiFunction<Cat, RadioactiveIsotope, SchrodingertCatBox>  start = ((c,a) -> {return new SchrodingertCatBox(c, a);});
-		
-		SchrodingertCatBox given = start.apply(new Cat(), new RadioactiveIsotope());
-		assertThat(given).isNotNull().isEqualTo(Experimentation.start(new Cat(), new RadioactiveIsotope()));
+		SchrodingertCatBox given = Experimentation.start(new Cat(), new RadioactiveIsotope());
+		assertThat(given).isNotNull();
 		
 		
 	}
