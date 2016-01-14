1ddeab4bc27b803be19bff15a3ade2a8fea231fc
ggorsontanguy <guillaume@gorson-tanguy.net>
2016-01-14 22:31:57 +0100

    may_kill_the_cat_when_open_the_box

--- schrodinger-s_cat/src/test/java/net/kata/SchrodingetCatBoxTest.java
+++ schrodinger-s_cat/src/test/java/net/kata/SchrodingetCatBoxTest.java
@@ -4,10 +4,10 @@ import static org.assertj.core.api.Fail.fail;
 
 import org.junit.Test;
 
-public class AppTest {
+public class SchrodingetCatBoxTest {
 
 	@Test
-	public final void test() {
+	public final void may_kill_the_cat_when_open_the_box() {
 		fail("Not yet implemented"); // TODO
 	}
 
